2eca9979f8786e7e2a90fba45362dfb6d49ec6d9
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-22 21:46:16 +0530

    [jay]: update task entity structure

--- src/main/java/com/gevernova/task_management_system/model/Task.java
+++ src/main/java/com/gevernova/task_management_system/model/Task.java
@@ -1,4 +1,34 @@
 package com.gevernova.task_management_system.model;
 
-public class Task {
+
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToOne
+    private Priority priority;
+
+    public enum Status { PENDING, IN_PROGRESS, DONE }
+
 }
