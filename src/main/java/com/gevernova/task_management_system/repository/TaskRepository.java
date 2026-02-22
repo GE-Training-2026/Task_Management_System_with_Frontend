1bd48a2dc071f5f145bfc5de86bd60f5769f47ed
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-22 21:47:25 +0530

    [jay]: update task repository

--- src/main/java/com/gevernova/task_management_system/repository/TaskRepository.java
+++ src/main/java/com/gevernova/task_management_system/repository/TaskRepository.java
@@ -1,4 +1,6 @@
 package com.gevernova.task_management_system.repository;
 
-public class TaskRepository {
-}
+import com.gevernova.task_management_system.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {}
